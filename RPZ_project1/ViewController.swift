449d93e146bede1e844643eb391486219e2e6628
Mezhik1337 <59055036+Mezhik1337@users.noreply.github.com>
2023-11-26 21:22:39 +0200

    Initial Commit

---
+++ RPZ_project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RPZ_project1
+//
+//  Created by Yura Mezhik on 26.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
